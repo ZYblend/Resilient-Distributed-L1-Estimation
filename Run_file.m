7fdc357fd1067b55bcd3540b94d6aaf149ef723a
ZYblend <36635562+ZYblend@users.noreply.github.com>
2023-10-03 19:01:25 -0400

    initialize the simualtion files

---
+++ Run_file.m
@@ -0,0 +1,70 @@
+%% Run file for Multi-camera Perception
+clear all
+clc
+
+%% Vehicle system Dynamics and controller
+n_states = 3;  %[x,y,theta]
+WheelBase=0.256;
+
+z0 = [-1; -2; 0];  % initial state
+constraint_steering =  pi/6;
+constraint_velocity = [-0.5, 0.5];
+
+load exampleMaps.mat;
+startLoc = [5 5];
+goalLoc = [12 3];
+
+C = eye(2,n_states);
+
+
+%% Camera system parameters
+num_agents = 4;
+n_meas = 3;
+
+% camera model parameters (calibrated)
+plane = 2.282825;   % height camera
+dir = pwd;
+% camParams = load(dir+"\nCamsParams.mat", "camParams");
+% camParams = camParams.camParams;
+extrinsics = load(dir+"\nCamsExtrinsics.mat", "camExtrinsics");
+extrinsics = extrinsics.camExtrinsics; % R|t
+% poses = load(dir+"\nCamsPoses.mat", "camPoses");
+% poses = poses.camPoses;
+
+% attack
+num_attack = 2;
+sparse_loc = [1,3];
+e = zeros(n_meas*num_agents,1);
+e(sparse_loc) = 2*rand(num_attack,1);
+
+%% Centralized estimation
+R_cent = zeros(num_agents*n_meas,n_states);
+t_cent = zeros(num_agents*n_meas,1);
+R_dist = cell(1,num_agents);
+for cam=1:num_agents
+    R_cent((cam-1)*n_meas+1:cam*n_meas,:) = extrinsics{cam}.R;
+    R_dist{1,cam} = extrinsics{cam}.R;
+    t_cent((cam-1)*n_meas+1:cam*n_meas,:) = extrinsics{cam}.Translation';
+end
+
+%% Distributed Perception parameters
+% communication topology
+Adj_mat = [0 1 0 0;
+           1 0 0 1;
+           0 0 0 1;
+           0 1 1 0];
+camGraph = graph(Adj_mat);
+figure;
+plot(camGraph);
+title('Camera Graph')
+
+% L = laplacian(camGraph); % laplacian matrix
+L = [1  -1  0  0;
+    -1   2  0  -1;
+     0   0  1  -1;
+     0  -1 -1  2];
+disp('Has Spanning Tree?')
+disp(num2str(eig(L)));
+
+% solver
+max_iter = 500;
---
+++ plot_file.m
@@ -0,0 +1,47 @@
+%% Plot file
+
+map = binaryOccupancyMap(simpleMap);
+robotPose = out.BicyclePose;
+estimatedPose = out.estimated_pose;
+estimatedPose = permute(estimatedPose,[1 3 2]);
+estimatedPose = reshape(estimatedPose,[],size(estimatedPose,2),1);
+estimatedPose = estimatedPose.';
+
+numRobots = size(robotPose, 2) / 3;
+thetaIdx = 3;
+
+% Translation
+xyz = robotPose;
+xyz(:, thetaIdx) = 0;
+
+% Rotation in XYZ euler angles
+theta = robotPose(:,thetaIdx);
+thetaEuler = zeros(size(robotPose, 1), 3 * size(theta, 2));
+thetaEuler(:, end) = theta;
+
+for k = 1:size(xyz, 1)
+    show(map)
+    hold on;
+    
+    % Plot Start Location
+    plotTransforms([startLoc, 0], eul2quat([0, 0, 0]))
+    text(startLoc(1), startLoc(2), 2, 'Start');
+    
+    % Plot Goal Location
+    plotTransforms([goalLoc, 0], eul2quat([0, 0, 0]))
+    text(goalLoc(1), goalLoc(2), 2, 'Goal');
+    
+    % Plot Robot's XY locations
+    plot(robotPose(:, 1), robotPose(:, 2), '-b')
+
+    % Plot stimated Robot's XY locations
+    plot(estimatedPose(:, 1), estimatedPose(:, 2), '--r')
+    
+    % Plot Robot's pose as it traverses the path
+    quat = eul2quat(thetaEuler(k, :), 'xyz');
+    plotTransforms(xyz(k,:), quat, 'MeshFilePath',...
+        'groundvehicle.stl');
+    
+    pause(0.01)
+    hold off;
+end
